a6a5316df87f97f03126e7cf700be1b00788c4b2
Brent Griffin <brent.griffin@dotcms.com>
2019-04-01 13:43:43 -0500

    updated to current version of google cloud

--- setup-02-create-storage.sh
+++ setup-02-create-storage.sh
@@ -5,4 +5,4 @@ gcloud beta compute disks create --size=200GB dbdata --zone us-central1-c
 gcloud beta compute disks create --size=200GB esstatic --zone us-central1-c
 
 # create NFS share
-gcloud beta filestore instances create nfs-server --tier=STANDARD --file-share=name="vol1",capacity=1TB --network=name="default" --location=us-central1-c
+gcloud beta filestore instances create nfs-server --tier=STANDARD --file-share=name="vol1",capacity=1TB --network=name="default" --zone=us-central1-c
